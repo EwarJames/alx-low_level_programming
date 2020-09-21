31384f7e222690f7b3a2f68c4b721e46768aaecb
EwarJames <ewaarjames@gmail.com>
2020-09-21 11:29:41 +0000

    Unix Simple genius

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+/*
+ * File: 101-quote.c
+ * Auth: Ewar James <ewaarjames@gmail.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "with proper grammar, but the outcome is a piece of art,
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+return (1);
+
+}
