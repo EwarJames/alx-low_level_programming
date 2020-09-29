716ad5bb45fd8162884457726175bb92e8c579ab
EwarJames <ewaarjames@gmail.com>
2020-09-29 09:36:39 +0000

    Reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ *rev_string - reverses a string
+ *@s: char array string type
+ *Return: no retrun
+ */
+void rev_string(char *s)
+{
+int k, c, l;
+char j;
+
+for (k = 0; s[k] != '\0'; k++)
+;
+l = k;
+
+for (k--, c = 0; c > l / 2; k--, c++)
+{
+
+j = s[c];
+s[c] = s[k];
+s[k] = j;
+
+}
+}
