94809078b4c690064e575f2bed6b10466a1653e8
EwarJames <ewaarjames@gmail.com>
2020-09-29 11:14:15 +0000

    Reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ *rev_string - Cause a string to be reversed
+ *@s: String to be reversed
+ */
+void rev_string(char *s)
+{
+int len = 0, index = 0;
+char m;
+
+while (s[index++])
+len++;
+for (index = len - 1; index >= len / 2; index--)
+{
+m = s[index];
+s[index] = s[len - index - 1];
+s[len - index - 1] = m;
+}
+
+}
