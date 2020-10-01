e3a7a7473bb37fa6e6a769b5753a1be9a9b553c6
EwarJames <ewaarjames@gmail.com>
2020-10-01 14:35:58 +0000

    Concatenate two strings with at most bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *_strncat - concatenates two strings
+ *@dest: destination string
+ *@src: source string
+ *@n: The number of bytes from src to be appended to dest
+ *Return: pointer to resulting `dest`
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int k, j;
+
+for (k = 0; dest[k] != '\0'; k++)
+;
+
+for (j = 0; src[j] != '\0' && n > 0; j++, n--, k++)
+{
+dest[k] = src[j];
+}
+return (dest);
+}
