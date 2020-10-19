eb156d1cbebea734fc9244d32db1cfe8b5797b5e
EwarJames <ewaarjames@gmail.com>
2020-10-19 09:49:42 +0000

    Header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - structure containing elements.
+ *@name: First member.
+ *@age: Second member.
+ *@owner: Third member.
+ *
+ *Description: Header file containing structure declarations.
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
