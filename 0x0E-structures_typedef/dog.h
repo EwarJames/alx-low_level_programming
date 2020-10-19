b4be935fce0a1b414cca8b94eb3ab856e012f55b
EwarJames <ewaarjames@gmail.com>
2020-10-19 12:22:58 +0000

    Header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,21 +2,26 @@
 #define DOG_H
 
 /**
- *struct dog - structure containing elements.
- *@name: First member.
- *@age: Second member.
- *@owner: Third member.
- *@d: structure pointer name.
- *
- *Description: Header file containing structure declarations.
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  */
-
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
